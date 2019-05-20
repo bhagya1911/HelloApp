a40b2feab73e4d07aae31906690171fc757babec
Bhagyalakshmi <https://lakshmibhagya@bitbucket.org>
2019-05-20 12:10:52 +0530

    myapp added

---
+++ KotlinApplication/app/src/main/java/com/example/kotlinapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        newfunction();
+    }
+
+    fun newfunction() {
+
+    }
+}
